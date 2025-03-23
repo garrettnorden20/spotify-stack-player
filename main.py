f0e3aa9adb7d5aa3f2ec0287b37882105f10259b
Garrett Norden <garrett.norden@appian.com>
2025-03-22 20:16:30 -0400

    added tracking, matched up w/ adafruit keys

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ load_dotenv()
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
-SPOTIFY_DEVICE_ID = os.getenv("SPOTIFY_DEVICE_ID")
 
 # Define the scope for playback control
 SCOPE = "user-modify-playback-state user-read-playback-state user-top-read"
@@ -35,8 +34,7 @@ class Controller:
         self.play_stack = []  # Initialize an empty list
         self.paused_time = None  # Initialize pausedTime as None
         self.paused_uri = None  # Initialize pausedUri as None
-        self.context_uri = None
-        self.device_id = SPOTIFY_DEVICE_ID
+        self.device_id = None
         self.last_song = None
         self.queued_song_uris = None
         self.stepped_uri = None
@@ -46,7 +44,6 @@ class Controller:
     def clear_paused(self):
         self.paused_time = None  # Initialize pausedTime as None
         self.paused_uri = None  # Initialize pausedUri as None
-        self.context_uri = None
         self.album_uri = None
 
 
@@ -66,9 +63,9 @@ def toggle_playback(sp: Spotify):
             sp.pause_playback()
             print("Playback paused.")
         else:
-            if controller.stepped_uri is None: # not in album
+            if controller.stepped_uri is None: # not in album, so resume from our list of queued
                 sp.start_playback(controller.device_id, None, controller.queued_song_uris, {"uri": controller.paused_uri}, controller.paused_time)
-            else:
+            else: # resume from within our album
                 sp.start_playback(controller.device_id, controller.album_uri, None, {"uri": controller.paused_uri}, controller.paused_time)
             controller.clear_paused()
             print("Playback resumed.")
@@ -111,11 +108,12 @@ def step_in(sp):
         controller.album_uri = curr_playback['item']['album']['uri']
         sp.start_playback(controller.device_id, controller.album_uri, None, None, None)
     except Exception as e:
-        print(f"Error going back to previous track: {e}")
+        print(f"Error stepping in: {e}")
 
 def queue_new(sp):
     curr_playback = sp.current_playback()
     try:
+        # if we had stepped into an album, resume from the song where we pressed "step"
         if controller.stepped_uri is not None and controller.queued_song_uris is not None and len(controller.queued_song_uris) > 0:
             print("Stepping out")
             sp.start_playback(controller.device_id, None, controller.queued_song_uris, {"uri": controller.stepped_uri}, None)
@@ -124,7 +122,7 @@ def queue_new(sp):
             return
         print("Queuing new songs")
 
-        size = 5
+        size = 30
         tracks = random.sample(get_all_top_tracks(sp), size)
         controller.queued_song_uris = tracks
         sp.start_playback(controller.device_id, None, tracks, None, None)
@@ -132,6 +130,23 @@ def queue_new(sp):
 
     except Exception as e:
         print(f"Error queuing new songs: {e}")
+
+def seek_track(sp: Spotify, offset_sec):
+    try:
+        curr_playback = sp.current_playback()
+        curr_pos_ms = curr_playback['progress_ms']
+        song_dur_ms = curr_playback['item']['duration_ms']
+        offset_sec_ms = offset_sec*1000
+        if curr_pos_ms + offset_sec_ms < 0:
+            sp.seek_track(0)
+        elif curr_pos_ms + offset_sec_ms > song_dur_ms:
+            return
+        else:
+            sp.seek_track(curr_pos_ms + offset_sec_ms)
+    except Exception as e:
+        print(f"Error adjusting song length: {e}")
+
+
 
 def get_all_top_tracks(sp, max_tracks=50, batch_size=50):
     tracks, offset = [], 0
@@ -147,17 +162,22 @@ def get_all_top_tracks(sp, max_tracks=50, batch_size=50):
 def on_press(event, sp):
     """Handle keypress events."""
     try:
-        if event.name == "f6":  # Replace with actual keys from the macropad
+        if event.name == "f13":  # Replace with actual keys from the macropad
             previous_track(sp)
-        elif event.name == "f8":
+        elif event.name == "f15":
             next_track(sp)
-        elif event.name == "f7":
+        elif event.name == "f20":
             toggle_playback(sp)
-        elif event.name == "f9":
+        elif event.name == "f14":
             step_in(sp)
-        elif event.name == "f10":
+        elif event.name == "f17":
             queue_new(sp)
-        # Add more key mappings as needed
+        elif event.name == "f16":
+            seek_track(sp, -10)
+        elif event.name == "f18":
+            seek_track(sp, 10)
+        elif event.name == "f23":
+            return # DJ???
     except Exception as e:
         print(f"Error handling key press: {e}")
 
@@ -165,6 +185,8 @@ def main():
     sp = get_spotify_client()
     print("Spotify controller started. Press keys to control playback:")
     print("F6: Previous Track, F8: Next Track, F7: Pause")
+
+    controller.device_id = sp.current_playback()['device']['id']
 
     # Set up keyboard hooks
     keyboard.on_press(lambda event: on_press(event, sp))
